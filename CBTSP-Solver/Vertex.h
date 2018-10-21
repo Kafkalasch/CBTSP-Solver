36aa7b07b66dcff0e31d3f8e00784a544009a96a
Claudius R <c.roehl@mathtec.at>
2018-10-21 10:48:41 +0200

    Added some comments.

--- CBTSP-Solver/Vertex.h
+++ CBTSP-Solver/Vertex.h
@@ -7,7 +7,7 @@ class Vertex
 public:
 	Vertex(int id) : id(id) {};
 
-	class const_iterator
+	class const_iterator //this makes Vertex feel like a real container; we can iterate trough his neighbours using range-based for-loops, e.g. (const auto& neighbour: vertex). 
 	{
 		using const_iterator_intern = std::unordered_map<const Vertex*, double>::const_iterator;
 	public:
@@ -18,7 +18,7 @@ public:
 		friend bool operator==(const_iterator l, const_iterator r) { return l.it == r.it; }
 	private:
 		std::unordered_map<const Vertex*, double>::const_iterator it;
-	};
+	}; 
 
 	const_iterator begin() const { return const_iterator(neighbours.begin()); }
 	const_iterator end() const { return const_iterator(neighbours.end()); }
@@ -31,18 +31,18 @@ public:
 	void eraseNeighbour(const Vertex& neighbour);
 private:
 	const int id;
-	std::unordered_map<const Vertex*, double> neighbours;
-	//auto getNeighbours() const -> const std::unordered_map<const Vertex*, double>& { return neighbours; }
+	std::unordered_map<const Vertex*, double> neighbours;  //Überlegung: vielleicht auch noch die inverse Abbildung abspeichern als multimap<double,const Vertex&> um die Suche nach kleinsten, bezw. größten Gewichten zu beschleunigen
+	//auto getNeighbours() const -> const std::unordered_map<const Vertex*, double>& { return neighbours; } //not nice
 };
 
-namespace std
-{
-	template<>
-	struct hash<Vertex>
-	{
-		size_t operator()(const Vertex& node) const
-		{
-			return hash<int>()(node.getId());
-		}
-	};
-}
+//namespace std
+//{
+//	template<>
+//	struct hash<Vertex>
+//	{
+//		size_t operator()(const Vertex& node) const
+//		{
+//			return hash<int>()(node.getId());
+//		}
+//	};
+//}
